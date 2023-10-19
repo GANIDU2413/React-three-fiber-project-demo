bd0de6d7d9cfe080b8eebe8723c7eef35b9c970e
GANIDU2413 <ganidusahan@gmail.com>
2023-10-19 23:55:40 +0530

    2355

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import logo from './logo.png';
+import {Canvas} from '@react-three/fiber';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +11,9 @@ root.render(
     <header>
       <img src={logo} alt='alt here'/>
     </header>
+    <Canvas>
+      <App />
+    </Canvas>
     <App />
   </React.StrictMode>
 );
