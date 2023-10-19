1846ca3c36f03dd08689686bfc650a374003d14c
GANIDU2413 <ganidusahan@gmail.com>
2023-10-19 23:45:31 +0530

    2345

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import logo from './logo.png';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <header>
+      <img src={logo} alt='alt here'/>
+    </header>
     <App />
   </React.StrictMode>
 );
